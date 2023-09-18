c575df4ff9cf227266b54e12ebdf390fe2513e3d
Adib <adiblab22@gmail.com>
2023-09-18 09:06:29 +0600

    write playround and computerchoice function

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 function getComputerChoice()
 {
-    var randomIndex = math.floor(math.random())
-    console.log(randomIndex)
+    var choices = ["rock" , "paper", "scissor"]
+    var randomIndex = Math.floor(Math.random()*3)
+    var coice = choices[randomIndex]
+    return coice
 }
 
-function playRound()
+function playRound(playerSelection,computerSelection)
 {
-
+    if(playerSelection===computerSelection){
+        return "tie"
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "you lose! paper beats rock"
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "you win! paper beats rock"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor"){
+        return "you win! rock beats scissor"
+    }
+    else if (playerSelection === "scissor" && computerSelection === "rock"){
+        return "you lose! rock beats scissor"
+    }
+    else if (playerSelection === "scissor" && computerSelection === "paper"){
+        return "you win! scissor beats paper"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "you lose! scissor beats paper"
+    }
 }
+
+console.log(playRound("paper","paper"))
